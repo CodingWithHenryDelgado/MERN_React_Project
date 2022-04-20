1ce171281e40765142560c9ef4e804f0f997bacf
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-04-19 22:43:43 -0400

    Created shipping address and save the address of the customer.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
-import RegisterScreen from './screens/RegisterScreen';
+import RegisterScreen from './screens/RegisterScreen'
+import ShippingScreen from './screens/ShippingScreen'
 import ProfileScreen from './screens/ProfileScreen'
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path="/shipping" component={ShippingScreen}/>
           <Route path="/login" component={LoginScreen}/>
           <Route path='/register' component={RegisterScreen}/>
           <Route path='/profile' component={ProfileScreen}/>
