db4e08389cb2bf3369beae852b2b3b48cf9ad54f
Henry_DelGlitch <CodingWHenry@gmail.com>
2023-03-09 12:59:45 -0500

    Update Search box placeholder text

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import { Form } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
 const SearchBox = ({ history }) => {
     const [keyword, setKeyword] = useState('')
+
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -19,7 +23,7 @@ const SearchBox = ({ history }) => {
                 type='text'
                 name='q'
                 onChange={(e) => setKeyword(e.target.value)}
-                placeholder='Search Products...'
+                placeholder={'Hi, ' + userInfo.name.split(' ')[0] + '! What can we help you find today?'}
                 className='mr-sm-2'
             ></Form.Control>
         </Form>
