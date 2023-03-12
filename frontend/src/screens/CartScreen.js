ae1169a1969b34d1dcc8e96025c9daf733ee0e48
Henry_DelGlitch <CodingWHenry@gmail.com>
2023-03-12 18:42:33 -0400

    Added comments to CartScreen. Changed screen of empty cart. Button will say Continue shopping when the cart is empty.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -8,35 +8,48 @@ import { addToCart, removeFromCart } from '../actions/cartActions'
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
 
+  // Get quantity from query string or default to 1
   const qty = location.search ? Number(location.search.split('=')[1]) : 1
 
   const dispatch = useDispatch()
 
+  // Get cart items from Redux store
   const cart = useSelector((state) => state.cart)
   const { cartItems } = cart
 
+  // Add item to cart if there is a productId and quantity
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
 
+  // Remove item from cart
   const removeFromCartHandler = (id) => {
     dispatch(removeFromCart(id))
   }
 
+  // Redirect to login page when user is not logged in
   const checkoutHandler = () => {
     history.push('/login?redirect=shipping')
+  }
+
+  // Redirect to homepage when cart is empty
+  const continueShopping = () => {
+    history.push('/')
   }
 
   return (
     <Row>
       <Col md={8}>
-        <h1>Shopping Cart</h1>
         {cartItems.length === 0 ? (
-          <Message>
-            Your cart is empty <Link to='/'>Go Back</Link>
-          </Message>
+          // Show message if cart is empty
+          <>
+            <h1>Your cart is empty</h1>
+            <Message>
+              Need a few ideas? <Link to='/'>Go to the homepage</Link>
+            </Message>
+          </>
         ) : (
           <ListGroup variant='flush'>
             {cartItems.map((item) => (
@@ -59,6 +72,7 @@ const CartScreen = ({ match, location, history }) => {
                         )
                       }
                     >
+                      {/* Show dropdown of available quantities */}
                       {[...Array(item.countInStock).keys()].map((x) => (
                         <option key={x + 1} value={x + 1}>
                           {x + 1}
@@ -82,28 +96,40 @@ const CartScreen = ({ match, location, history }) => {
         )}
       </Col>
       <Col md={4} className='subtotal'>
+        {/* Show subtotal and checkout button */}
         <Card>
           <ListGroup variant='flush'>
             <ListGroup.Item>
               <h2>
-                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
-                items
+                Order Summary
               </h2>
               $
               {cartItems
                 .reduce((acc, item) => acc + item.qty * item.price, 0)
                 .toFixed(2)}
             </ListGroup.Item>
-            <ListGroup.Item>
-              <Button
-                type='button'
-                className='btn-block'
-                disabled={cartItems.length === 0}
-                onClick={checkoutHandler}
-              >
-                Proceed To Checkout
-              </Button>
-            </ListGroup.Item>
+            {
+              cartItems.length === 0 ?
+                <ListGroup.Item>
+                  <Button
+                    type='button'
+                    className='btn-block'
+                    onClick={continueShopping}
+                  >
+                    Continue Shopping
+                  </Button>
+                </ListGroup.Item>
+                :
+                <ListGroup.Item>
+                  <Button
+                    type='button'
+                    className='btn-block add-to-cart'
+                    onClick={checkoutHandler}
+                  >
+                    Proceed To Checkout
+                  </Button>
+                </ListGroup.Item>
+            }
           </ListGroup>
         </Card>
       </Col>
